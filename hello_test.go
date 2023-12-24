fda140b9f50bce6f10f174a008a12f02364424cb
bailey-manjaro <kongbiying2022@email.szu.edu.cn>
2023-12-24 16:11:14 +0800

    feat: test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	got:=Hello("kb")
-	want := "Hello, kb"
+	
+	t.Run("saying hello to people", func(t *testing.T) {
+		got:=Hello("kb")
+		want := "Hello, kb"
+	
+		if got!= want{
+			t.Errorf("got '%q' want '%q'",got, want)
+		}
+	})
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got:=Hello("")
+		want:= "Hello, world"
 
-	if got!= want{
-		t.Errorf("got '%q' want '%q'",got, want)
-	}
+		if got != want{
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
 }
