e78886f60a1b573397fddde2dd6f0dce1e86954e
bailey-manjaro <kongbiying2022@email.szu.edu.cn>
2024-01-03 20:16:56 +0800

    struct

---
+++ shapes_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"math"
+	"testing"
+)
+
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+type Circle struct{
+	Radius float64
+}
+type Shape interface{
+	Area() float64
+}
+
+func Perimeter(width float64, height float64) float64{
+	return 2*(width + height)
+}
+
+func TestPerimeter(t * testing.T){
+	rectangle := Rectangle{10.0, 10.0} 
+	got:= Perimeter(rectangle.Width, rectangle.Height)
+	want:= 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T){
+	t.Run("retangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0,6.0} 
+		got:=rectangle.Area()
+		want:= 72.0
+	
+		if got!=want{
+			t.Errorf("got %.2f want %.2f", got ,want)
+		}
+	})
+	t.Run("cycle", func(t *testing.T) {
+		circle := Circle{10} 
+		got:=circle.Area()
+		want:= 314.1592653589793
+	
+		if got!=want{
+			t.Errorf("got %.2f want %.2f", got ,want)
+		}
+	})
+
+	
+}
+
+func (r Rectangle) Area() float64{
+	return r.Height *r.Width
+}
+
+func (c Circle) Area() float64{
+	return c.Radius*c.Radius*math.Pi
+}
+
+// func Area(width float64, height float64) float64{
+// 	return width * height
+// }
+// func Area(radius float64) float64{
+// 	return radius * radius
+// }
